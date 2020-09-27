77e37a31de48eae1f742a2937ae56aee5da93a53
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 12:53:59 +0800

    >  compile NEMU 3019244148 lhx Linux ubuntu 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:53:59 up  1:41,  1 user,  load average: 0.16, 0.23, 0.13 169ef74dc1aa05c7bad5ac7035ca11dcd8ff0982

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -67,7 +67,7 @@ static int cmd_x(char *args) {
 	sscanf(AD, "%x", &ad);
 	printf("0x%x: ", ad);
 	for (i = 0; i < n; i++) {
-		printf("%x", hwaddr_read(ad, 4));
+		printf("%x ", hwaddr_read(ad, 4));
 		ad += 4;
 	}
 	printf("\n");
